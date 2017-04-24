8805b106c19e4a59117424c9a1bd1c13c55a378a
Alex Reed <alexreed6899@gmail.com>
2017-04-23 19:13:28 -0500

    Update RPi_Final.py

--- RPi_Final.py
+++ RPi_Final.py
@@ -1,1 +1,5 @@
-from random import *
+###############################################################################################################################
+#Collaborators: Jordan Edgel, Kaleb Rhody, Thomas Presicci, Alex Reed
+#Description: A series of trivia questions asked entirely in Morse Code to be solved by competitors. Players are scored by time.
+#Due Date: May 10, 2017
+###############################################################################################################################
