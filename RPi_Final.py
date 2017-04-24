366b4e692341b67801ab163c8b6b8d71eed4815d
Alex Reed <alexreed6899@gmail.com>
2017-04-23 19:16:07 -0500

    Update RPi_Final.py

--- RPi_Final.py
+++ RPi_Final.py
@@ -3,3 +3,6 @@
 #Description: A series of trivia questions asked entirely in Morse Code to be solved by competitors. Players are scored by time.
 #Due Date: May 10, 2017
 ###############################################################################################################################
+from time import *
+import pygame
+import RPi.GPIO as GPIO
