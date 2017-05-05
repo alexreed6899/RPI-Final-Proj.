9ceea4285100bff26bb2b15f97be35df50f82138
RhodyK <kaleb_rhody@hotmail.com>
2017-05-05 01:03:13 -0500

    Update RPi_Final.py

--- RPi_Final.py
+++ RPi_Final.py
@@ -4,10 +4,17 @@
 #Due Date: May 10, 2017
 ###############################################################################################################################
 from time import *
-import pygame
-import RPi.GPIO as GPIO
+#import pygame
+#import RPi.GPIO as GPIO
 from Tkinter import *
+import threading
 
+# Dot = one unit / Dash = three units
+# Space between digits = one unit
+# Space between characters = three units
+# Space between words = seven units
+
+# Delete last word = eight dots
 
 class Game(Canvas):
         def __init__(self, master):
@@ -16,13 +23,35 @@ class Game(Canvas):
                 master.columnconfigure(0, weight = 1)
                 master.rowconfigure(0, weight = 1)
                 
+                # List of questions in form of 1s and 0s
+                # 0 = dash, 1 = dot
+                self.questions = ['.--/..../../-.-./.... .-./---/...-/./.-. .--/.-/... -/..../. ..-./../.-./.../- -/--- ./-..-/.--./.-../---/.-./. --/.-/.-./...', # Question 1, [Which rover was the first to explore Mars? - Sojourner]
+                             '..../---/.-- --/..-/-.-./.... -/../--/. -../---/./... .- .../..-/-. .-./.-/-.-- -/.-/-.-/. -/--- -/.-./.-/...-/./.-.. -/--- ./.-/.-./-/....', # Question 2, [How much time does a sun ray take to travel to Earth? - 8 minutes]
+                             '--/../.-../-.-/-.-- .--/.-/-.-- -.../.-../.-/-.-./-.- ..../---/.-../. -../../.../-/.-/-./-.-./. ../-. .-../../--./..../- -.--/./.-/.-./...', # Question 3, [Milky Way black hole distance in light years? - 27,000 light years]
+                             '.--/..../.-/- ../... -/..../. .../..-/-. --/---/.../-/.-../-.-- --/.-/-../. ---/..-.'] # Question 4, [What is the sun mostly made of? - Hydrogen]
+
+                self.questions = ['.','-','.','-']
+                
+                # Dict for the times of each dot and dash
+                self.morse = {'-':UNIT*3, '.':UNIT}
+
+                # Used to switch the LED the word is displayed on
+                self.leds = {RED:GREEN, GREEN:RED}
+                
+                # Initialized variables
+                self.time = StringVar()
+                self.current_question = 0
+                self.current_word = -1
+                self.current_led = RED
+                self.answering = False
+
         def gui(self):
                 self.grid()
                 
-                img = PhotoImage( file = "neil.gif" )
-                self.pic = Label(self.master, image = img, height = 40, width = 40)
-                self.pic.image = img
-                self.pic.grid(row = 0, column = 0, sticky = N+S+E+W)
+                img = PhotoImage( file = "Neil.gif" )
+                pic = Label(self.master, image = img)
+                pic.image = img
+                pic.grid(row = 0, column = 0, sticky = N+S+E+W)
 
                 self.ansA = Radiobutton(self.master, text = atext, value = 1, variable = ansvar, width = 10)
                 self.ansA.grid(row = 1, column = 0)
@@ -33,55 +62,192 @@ class Game(Canvas):
                 self.ansD = Radiobutton(self.master, text = dtext, value = 4, variable = ansvar, width = 10)
                 self.ansD.grid(row = 2, column = 1)
 
-                self.timer = Label(self.master, text = "Time: ", state = DISABLED, height = 5, width = 25)
+                self.timer = Label(self.master, textvariable = self.time, state = DISABLED)
                 self.timer.grid(row = 0, column = 2, columnspan = 2, sticky = W)
 
-                self.qNumber = Label(self.master, text = "Question #", state = DISABLED, height = 5, width = 25)
+                self.qNumber = Label(self.master, text = "Question # {}".format(self.current_question + 1), state = DISABLED)
                 self.qNumber.grid(row = 1, column = 2, columnspan = 2, sticky = N+W)
 
-                self.submit = Button(self.master, text = testvar, height = 2, width = 10, command = self.checkAns)
+                self.nextWord = Button(self.master, text = "Next Word", command = self.buttonOne)
+                self.nextWord.grid(row = 2, column = 2)
+
+                self.replayWord = Button(self.master, text = "Replay Word", command = self.buttonTwo)
+                self.replayWord.grid(row = 2, column = 3)
+
+                self.submit = Button(self.master, text = "Submit Answer", command = self.buttonThree)
                 self.submit.grid(row = 3, column = 2, columnspan = 2)
 
         def play(self):
+                self.setText()
                 self.gui()
+
+        def buttonOne(self):
+                self.current_word += 1
+                self.current_led = self.leds[self.current_led]
+                print 'Current word: {}'.format(self.current_word)
+                print 'Current question: {}'.format(self.current_question)
+                self.runQuestion()
+
+        def buttonTwo(self):
+                print 'Current word: {}'.format(self.current_word)
+                print 'Current question: {}'.format(self.current_question)
+                self.runQuestion()
+
+        def buttonThree(self):
+                if not self.answering:
+                        print "The question hasen't been fully asked yet."
+                        return
+                self.checkAns(ansvar.get())
+                
+
         
-        def morsestuff(self):
-                pass
+        def runQuestion(self):
+                words = self.questions[self.current_question].split(' ')
+                if (len(words)-1) < self.current_word:
+                        print 'This is the end of the question its time to answer.'
+                        return
+                #GPIO.output(current_led, True)
+                for n in words[self.current_word]:
+                        if n != '/':
+                                self.blink(n, self.current_led)
+                        else:
+                                sleep(UNIT*3)
+                                print 'end of character'
+                #GPIO.output(current_led, False)
+                if (len(words)-1) == self.current_word:
+                        self.answering = True
+                        
 
-        def ledstuff(self):
-                pass
+        # Handles the blinking duration and led
+        def blink(self, length, current_led):
+                if length in self.morse:
+                        print 'light {}, {}'.format(length, current_led)
+                        #GPIO.output(current_led, True)
+                        sleep(self.morse[length])
+                        #GPIO.output(current_led, False)
+                        #sleep(UNIT)
+                else:
+                        print 'blink.error({}, {})'.format(length, current_led)
 
-        def timerstuff(self):
-                pass
-        
-        def checkAns(self):
-            if(ansvar.get() == 1):
-                testvar = "ans 1"
-            elif(ansvar.get() == 2):
-                testvar = "Ans 2"
-            elif(ansvar.get() == 3):
-                testvar = "Ans 3"
-            elif(ansvar.get() == 4):
-                testvar = "Ans 4"
-            print testvar
+        def setText(self):
+                if (self.current_question == 0):
+                        atext = "Sojourner"
+                        btext = "Curiosity"
+                        ctext = "MER"
+                        dtext = "Prop-M"
+                elif (self.current_question == 1):
+                        atext = "20 minutes"
+                        btext = "1 minute"
+                        ctext = "8 minutes"
+                        dtext = "40 minutes"
+                elif (self.current_question == 2):
+                        atext = "10,000 light years"
+                        btext = "27,000 light years"
+                        ctext = "15,000 light years"
+                        dtext = "45,000 light years"
+                elif (self.current_question == 3):
+                        atext = "Carbon"
+                        btext = "Water"
+                        ctext = "Iron"
+                        dtext = "Hydrogen"
 
-atext = "placeholder a"
-btext = "placeholder b"
-ctext = "placeholder c"
-dtext = "placeholder d"
-testvar = ""
+        def checkAns(self, var):
+                if (self.current_question == 0):
+                        if (var == 1):
+                                print "Correct Answer!"
+                                self.current_question += 1
+                                self.current_word = -1
+                                self.answering = False
+                                self.setText()
+                                self.gui()
+                        else:
+                                print "Wrong Answer!"
+
+                elif (self.current_question == 1):
+                        if (var == 3):
+                                print "Correct Answer!"
+                                self.current_question += 1
+                                self.current_word = -1
+                                self.answering = False
+                                self.setText()
+                                self.gui()
+                        else:
+                                print "Wrong Answer!"
+
+                elif (self.current_question == 2):
+                        if (var == 2):
+                                print "Correct Answer!"
+                                self.current_question += 1
+                                self.current_word = -1
+                                self.answering = False
+                                self.setText()
+                                self.gui()
+                        else:
+                                print "Wrong Answer!"
+
+                elif (self.current_question == 3):
+                        if (var == 4):
+                                print "Correct Answer!"
+                                self.current_question += 1
+                                self.current_word = -1
+                                self.answering = False
+                                self.setText()
+                                self.gui()
+                                time = timer['text']
+                                s = score(time, penalty)
+                                #showScore(s)
+                        else:
+                                print "Wrong Answer!"
+
+        def updateTime(self, a):
+                self.time.set("Time: " + str(a) + " sec")
+                print self.time
+
+def score(t, penalty):
+        playing = False
+        return t+penalty
+
+def timer():
+        t = 0
+        while(playing == True):
+                t += 1
+                f.updateTime(t)
+                sleep(1)
+
+
+# Constants // assuming the green led is for every other word
+RED = 12
+GREEN = 17
+RGB_RED = 22
+RGB_GREEN = 21
+RGB_BLUE = 4
+REPEAT = 6
+NEXT_WORD = 26
+UNIT = .2
+
+playing = True
+atext = "aaa"
+btext = "bbb"
+ctext = "ccc" 
+dtext = "ddd"
+penalty = 0
+time = threading.Thread(target = timer)
+time.deamon = True
+
+# GPIO stuffs
+#GPIO.setmode(GPIO.BCM)
+
+#GPIO.setup(red, GPIO.OUT)
+#GPIO.setup(green, GPIO.OUT)
+
+#GPIO.setup(repeat, GPIO.IN, GPIO.PUD_DOWN)
+#GPIO.setup(next_word, GPIO.IN, GPIO.PUD_DOWN) 
+
 window = Tk()
 ansvar = IntVar()
-
-questdict = {1:3, 2:2, 3:4, 4:1}
-question1 = []
-question2 = []
-question3 = []
-question4 = []
-
 window.title("Space with Tyson")
-window.geometry("500x400")
+window.geometry("600x500")
 f = Game(window)
 f.play()
+time.start()
 window.mainloop()
-
